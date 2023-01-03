1b96013b2240406ee12149137a9ecc19c7b6adf8
dineshNehete <nehetedinesh7@gmail.com>
2023-01-04 02:54:17 +0530

    Meal Activity Layout created

---
+++ app/src/main/java/com/example/recipeapp/activities/MealActivity.kt
@@ -0,0 +1,12 @@
+package com.example.recipeapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.recipeapp.R
+
+class MealActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_meal)
+    }
+}
--- app/src/main/java/com/example/recipeapp/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/recipeapp/fragments/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.recipeapp.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,16 +9,16 @@ import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
+import com.example.recipeapp.activities.MealActivity
 import com.example.recipeapp.databinding.FragmentHomeBinding
 import com.example.recipeapp.pojo.Meal
 import com.example.recipeapp.viewmodel.HomeViewModel
 
 
-
 class HomeFragment : Fragment() {
     private var binding: FragmentHomeBinding? = null
 
-    private lateinit var homeMvvm : HomeViewModel
+    private lateinit var homeMvvm: HomeViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = FragmentHomeBinding.inflate(layoutInflater)
@@ -48,10 +49,22 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         homeMvvm.getRandomMeal()
         observerRandomMeal()
+        onRandomMealClick()
     }
 
-    private fun observerRandomMeal(){
-        homeMvvm.observeRandomMealLiveData().observe(viewLifecycleOwner, object : Observer<Meal>{
+    private fun onRandomMealClick() {
+        binding?.cvRandomMeal?.setOnClickListener {
+            val intent = Intent(activity, MealActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+
+    /**
+     * When the view is created and the call is made, load the data into the image view
+     */
+    private fun observerRandomMeal() {
+        homeMvvm.observeRandomMealLiveData().observe(viewLifecycleOwner, object : Observer<Meal> {
             override fun onChanged(meal: Meal?) {
                 if (meal != null) {
                     Glide.with(this@HomeFragment).load(meal.strMealThumb)
